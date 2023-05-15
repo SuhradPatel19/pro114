6be21d190eab28daa82ad75f749a9601eb4401be
SuhradPatel19 <100881431+SuhradPatel19@users.noreply.github.com>
2023-05-15 16:07:11 -0400

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,11 @@
+a = 33
+b = "33"
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+# the code doesn't work because b is a string
+# to fix change be to an int by getting rid of the ""
+
---
+++ test2.py
@@ -0,0 +1,7 @@
+def myFunction(fname):
+ print(fname + "Refsnes")
+
+
+myFunction("Emil")
+myFunction("Tosbais")
+myFunction("Linus")
